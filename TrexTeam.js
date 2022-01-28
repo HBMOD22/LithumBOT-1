bcba1edd540cdaa340430a7c62810d1785e4d38c
HBMOD22 <93594447+HBMOD22@users.noreply.github.com>
2022-01-28 10:29:41 +0530

    🐯TrexTeam.js🐯

---
+++ TrexTeam.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Lithum_OWNER: '94784506970', // Trex Team - Lithum Owner
+    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
+    TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Lithum 
+};
